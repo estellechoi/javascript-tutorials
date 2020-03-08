1f88132a7951ae5f24715b34e84700fed7124048
Estelle Choi <estele.choi@gmail.com>
2020-03-08 20:22:10 +0900

    Arrow Function

---
+++ ECMA2015/arrowFunction.js
@@ -0,0 +1,9 @@
+// Arrow Function (Fat Arrow)
+var getSum = (a, b) => {
+	return a + b;
+};
+
+console.log(getSum(2, 3));
+
+var arr = ["a", "b", "c"];
+arr.forEach((value, index) => console.log(value, index));
